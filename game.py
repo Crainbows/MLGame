995a9e334108b038dbb7281431404049afb59d28
Oliver Balmford <oli@oli.codes>
2019-02-21 11:01:47 +0000

    Add acceleration, braking and infinite edges

--- game.py
+++ game.py
@@ -11,8 +11,8 @@ fps_display.label.font_size = 50
 main_batch = pyglet.graphics.Batch()
 player_image = pyglet.image.load('res/Car_1_01.png')
 player = pyglet.sprite.Sprite(player_image, x=50, y=50, batch=main_batch)
-player.scale_y = 0.05
-player.scale_x = 0.05
+player.scale_y = 0.03
+player.scale_x = 0.03
 
 player.image.anchor_x = player.image.width / 2
 player.image.anchor_y = player.image.height / 2
@@ -21,8 +21,9 @@ right = False
 left = False
 forward = False
 backward = False
-player_speed = 300
-player_turning_speed = 2
+player_max_speed = 400
+player_speed = 0
+player_turning_speed = 5
 
 
 @window.event
@@ -58,19 +59,48 @@ def on_draw():
 
 def update(dt):
     player_move(player, dt)
+    player_drive(player, dt)
+    check_player_bounds(player)
 
 def player_move(entity, dt):
-    if forward and entity.y < 785:
+    if player_speed > 0:
         entity.y += math.cos(math.radians(entity.rotation % 360)) * player_speed * dt
         entity.x += math.sin(math.radians(entity.rotation % 360)) * player_speed * dt
-    if backward and entity.y > 0:
-        entity.y -= math.cos(math.radians(entity.rotation % 360)) * player_speed * dt
-        entity.x -= math.sin(math.radians(entity.rotation % 360)) * player_speed * dt
-    if (left and forward) or (right and backward):
-        entity.rotation -= player_turning_speed
-    if (right and forward) or (left and backward):
-        entity.rotation += player_turning_speed
+        if left:
+            entity.rotation -= player_turning_speed
+        if right:
+            entity.rotation += player_turning_speed
 
+    elif player_speed < 0:
+        entity.y += math.cos(math.radians(entity.rotation % 360)) * player_speed * dt
+        entity.x += math.sin(math.radians(entity.rotation % 360)) * player_speed * dt
+        if right:
+            entity.rotation -= player_turning_speed
+        if left:
+            entity.rotation += player_turning_speed
+
+def player_drive(entity, dt):
+    global player_speed
+    if forward and player_speed < player_max_speed:
+        player_speed += 20
+    elif backward and player_speed > -player_max_speed:
+        player_speed -= 20
+    elif player_speed != 0:
+        player_speed -= int(math.copysign(5, player_speed))
+
+def check_player_bounds(entity):
+    min_x = -(entity.image.width * entity.scale_x) / 2
+    min_y = -(entity.image.height * entity.scale_y) / 2
+    max_x = 1200 + (entity.image.width * entity.scale_x) / 2
+    max_y = 900 + (entity.image.height * entity.scale_y) / 2
+    if entity.x < min_x:
+        entity.x = max_x
+    elif entity.x > max_x:
+        entity.x = min_x
+    if entity.y < min_y:
+        entity.y = max_y
+    elif entity.y > max_y:
+        entity.y = min_y
 
 
 if __name__ == "__main__":
